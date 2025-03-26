fb1fa768ab9fbdc2b49cca79f07e3664ec0304d1
Joao Paulo <joaopaulobrandao2708@gmail.com>
2025-03-26 10:55:35 -0300

    codigos das aulas 3 e 4

--- aulas/aula03/tipos_de_dados.c
+++ aulas/aula03/tipos_de_dados.c
@@ -8,9 +8,9 @@ int main(){
 //void sem tipo
 
 printf("O tipo 'char'ocupa %i bytes e vai de %i a %i\n",sizeof(char),-127,128);
-printf("O tipo 'int'ocupa %i bytes e vai de %i a %i\n",sizeof(char),-212783648,212783647);
-printf("O tipo 'float'ocupa %i bytes e vai de %E a %E\n",sizeof(char),-3.4E+38,3.4+38);
-printf("O tipo 'double'ocupa %i bytes e vai de %LE a %LE\n",sizeof(char),-1.8E+300,1.8E+300);
+printf("O tipo 'int'ocupa %i bytes e vai de %i a %i\n",sizeof(int),-212783648,212783647);
+printf("O tipo 'float'ocupa %i bytes e vai de %E a %E\n",sizeof(float),-3.4E+38,3.4+38);
+printf("O tipo 'double'ocupa %i bytes e vai de %LE a %LE\n",sizeof(double),-1.8E+300,1.8E+300);
 printf("O tipo 'void'ocupa %i bytes\n",sizeof(void));
 return 0;
 }
