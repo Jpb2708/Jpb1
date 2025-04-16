6b94979fcad9c3dc820624bfeb7d0afb8ddbb231
Joao Paulo <joaopaulobrandao2708@gmail.com>
2025-04-16 10:58:45 -0300

    codigo da aula 06

---
+++ aulas/aula06/posso_votar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+int idade = 0;
+
+printf("informe sua idade: ");
+scanf("%i",&idade);
+if(idade < 16) {
+    printf("Voce naum pode votar!\n");
+} else {
+    if(idade >= 18) {
+        printf("Voce eh obrigado a votar!\n");
+    } else {
+    printf("Voce pode votar!\n");
+    }
+}
+    return 0;
+}
