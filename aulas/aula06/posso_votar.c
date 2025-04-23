07ad1509621e34761041a6700c1f2a95976624ce
Joao Paulo <joaopaulobrandao2708@gmail.com>
2025-04-23 10:53:57 -0300

    codigos da aula e praticas 6

--- aulas/aula06/posso_votar.c
+++ aulas/aula06/posso_votar.c
@@ -5,6 +5,7 @@ int idade = 0;
 
 printf("informe sua idade: ");
 scanf("%i",&idade);
+
 if(idade < 16) {
     printf("Voce naum pode votar!\n");
 } else {
@@ -14,5 +15,47 @@ if(idade < 16) {
     printf("Voce pode votar!\n");
     }
 }
+
+int idade;
+printf("Digite sua idade: ");
+scanf("%i", &idade);
+
+if (idade < 16) {
+    printf("Voce naum pode votar!\n");
+  } else {
+    if (idade < 18 || idade > 70) {
+      printf("Voce pode votar!\n");
+    }
+  } 
+  else {
+    printf("Voce eh obrigado a votar!\n"); 
+  }
+
+ int media;
+  printf("Digite sua media final: ");
+  scanf("%f", &media);
+  
+  if (media == 0.0f) {
+    printf("A mencao eh 'SR'.\n");
+  } else if (media < 3.0f) {
+    printf("A mencao eh 'II'.\n");
+  } else if (media < 5.0f) {
+    printf("A mencao eh 'MI'.\n");
+  } else if (media < 7.0f) {
+    printf("A mencao eh 'MM'.\n");
+  } else if (media < 9.0f) {
+    printf("A mencao eh 'MS'.\n");
+  } else {
+    printf("A mencao eh 'SR'.\n");
+  }
+  
+  
+  
+  
+  
+  
+     
+  
+
     return 0;
 }
---
+++ praticas/praticas06/ano_bissexto.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    
+    int ano;
+    printf("digite um ano: ");
+  scanf("%.i", &ano )
+
+    return 0;
+}
