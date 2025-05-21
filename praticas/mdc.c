666d8cf87c0160e3344af5f96cb7a87d25db0f5b
Joao Paulo <joaopaulobrandao2708@gmail.com>
2025-05-21 10:45:25 -0300

    Adicionando arquivos da pratica08

---
+++ praticas/mdc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1 =0;
+    int numero2=0;
+    
+    printf("Escreva o numero1: ");
+    scanf("%i", &numero1);
+    getchar();
+    
+    printf("Escreva o numero2: ");
+    scanf("%i", &numero2);
+    getchar();
+
+    int Q = numero1/numero2
+    int Resto = numero1
+    
+
+    return 0;
+}
